5cfde19f8c44c3e050f1b49d6733ad2f9c13f2c7
lofty87 <lofty87@daum.net>
2023-09-14 12:30:18 +0900

    v2.0.5 (prettier: v1.0.3, eslint: v3.0.5)
    1. jsx-a11y/no-autofocus rule off 처리
    2. v2.0.4 (prettier: v1.0.3, eslint: v3.0.4) 건너띄기 (npm publish error)

--- eslint-config/rules/jsx-a11y.js
+++ eslint-config/rules/jsx-a11y.js
@@ -7,7 +7,7 @@ module.exports = {
     'jsx-a11y/html-has-lang': 'error',
     'jsx-a11y/iframe-has-title': 'error',
     'jsx-a11y/lang': 'error',
-    'jsx-a11y/no-autofocus': 'error',
+    'jsx-a11y/no-autofocus': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'error',
     'jsx-a11y/no-redundant-roles': 'error',
     'jsx-a11y/no-static-element-interactions': 'off',
